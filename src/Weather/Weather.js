1b9bfcd9eb550ce8a9eb13145347474bc3529527
Zobaer Ahmed Zihad <zobaerahmedzihad02@gmail.com>
2022-07-28 13:31:09 +0600

    weather app is complete.

--- src/Weather/Weather.js
+++ src/Weather/Weather.js
@@ -1,47 +1,61 @@
-import { Axios } from "axios";
+import axios, { Axios } from "axios";
 import React, { useState } from "react";
 import "./Weather.css";
 
 const Weather = () => {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=f0e6cfc9ad3d1738cbe9476f6b35e1d4`;
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=f0e6cfc9ad3d1738cbe9476f6b35e1d4`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=f0e6cfc9ad3d1738cbe9476f6b35e1d4&units=metric`;
+
+  const { name, main, wind } = data;
 
   const searchLocation = (event) => {
     if (event.key === "Enter") {
-      Axios.get(url).then((response) => {
+      axios.get(url).then((response) => {
         setData(response.data);
         console.log(response.data);
       });
+      setLocation('')
     }
   };
+  console.log(data, "dta");
 
   return (
     <div className="weather__container">
-      
+      <div className="search">
+        <input
+          value={location}
+          placeholder="Enter Location"
+          autoComplete="off"
+          onChange={(event) => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          type="text"
+        />
+      </div>
       <div className="container">
         <div className="top">
           <div className="location">
-            <p>Dhaka</p>
+            <p>{data.name ? name : "City Name"}</p>
           </div>
           <div className="temp">
-            <h1>70 °F</h1>
+            <h1>{data.main ? data.main?.temp : "00"} °C</h1>
           </div>
           <div className="description">
-            <p>Clouds</p>
+            <p>{data.weather ? data?.weather[0]?.main : "weather"}</p>
           </div>
         </div>
         <div className="bottom">
           <div className="feels">
-            <p className="bold"> 65 °F</p>
+            <p className="bold"> {data.main ? main?.feels_like : "00"} °C</p>
             <p>Feels Like</p>
           </div>
           <div className="humidity">
-            <p className="bold">20%</p>
+            <p className="bold">{data.main ? main?.humidity : "00"} %</p>
             <p>Humidity</p>
           </div>
           <div className="wind">
-            <p className="bold">12 MPH</p>
+            <p className="bold"> {data.wind ? wind?.speed : "00"} MPH</p>
             <p>Wind Speed</p>
           </div>
         </div>
